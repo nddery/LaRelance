c88c092676ead8bab1156aa8b3aaedcb74b9ea17
Nicolas Duvieusart Dery <nddery@gmail.com>
2013-02-16 11:01:10 -0500

    Init of the data is here

--- js/main.js
+++ js/main.js
@@ -2,18 +2,5 @@
   // Create or open the database.
   App.data.init();
 
-  // App.data.retrieveData(
-  //   App.data.store.universite,
-  //   'http://mysites.dev/nddery.ca_www/larelance/data/universite.json'
-  // );
-
-  // App.data.retrieveData(
-  //   App.data.store.programmes,
-  //   'http://mysites.dev/nddery.ca_www/larelance/data/programmes.json'
-  // );
-
-  // App.data.retrieveData(
-  //   App.data.store.donnees,
-  //   'http://mysites.dev/nddery.ca_www/larelance/data/donnees.json'
-  // );
+  // Ensure the SVG element is has big has it can be.
 })();
